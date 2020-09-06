b3eee7b8751e435b832ba2b0d42cc8284ca4d679
wlukla <vladislav.danilochkin@gmail.com>
2020-09-06 13:44:22 +0300

    feat: set up todo checking and deleting functionality

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
+    <h1>#TODO</h1>
+    <TodoList
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
   </div>
 </template>
 
 <script>
+import TodoList from '@/components/TodoList';
+
 export default {
   name: 'App',
+  data: () => ({
+    todos: [
+      {id: 1, title: 'Read some book', completed: false},
+      {id: 2, title: 'Write some vue code', completed: false},
+      {id: 3, title: 'Write some golang code', completed: false},
+      {id: 4, title: 'Cook delicios meal', completed: false},
+    ],
+  }),
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id != id)
+    },
+  },
   components: {
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <li>
+      <span v-bind:class="{done: todo.completed}">
+        <input
+          type="checkbox"
+          v-on:change="todo.completed = !todo.completed"
+        />
+        <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+      </span>
+      <button
+        class="delete-btn"
+        v-on:click="$emit('remove-todo', todo.id)"
+        >
+          &times;
+        </button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    margin: 0 1rem;
+  }
+
+  .delete-btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #ff0000;
+    color: #ffffff;
+    border-radius: 50%;
+    font-weight: 800;
+    font-size: 1.2rem;
+    width: 2rem;
+    height: 2rem;
+    border: none;
+    cursor: pointer;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
